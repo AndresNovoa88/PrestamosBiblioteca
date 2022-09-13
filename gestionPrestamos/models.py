f04411fab62054ad4e09e799b51a18d306abd60c
AndresNovoa88 <andresnovoa334@gmail.com>
2022-09-13 11:59:17 -0500

    Version:Models y Views

---
+++ gestionPrestamos/models.py
@@ -0,0 +1,27 @@
+from pickle import FALSE
+from django.db import models
+
+# Create your models here.
+
+class Libro(models.Model):
+    Isbn=models.CharField(max_length=25,primary_key=True)
+    titulo=models.TextField(max_length=100,unique=True,null=False)
+    editorial=models.CharField(max_length=50,null=False)
+    autor=models.CharField(max_length=50,null=False)
+    no_page=models.IntegerField(null=True)
+    
+class Estudiante(models.Model):
+    documento=models.IntegerField(primary_key=True)
+    nombre=models.TextField(max_length=50,null=True)
+    apellido=models.TextField(max_length=50,null=False)
+    email=models.EmailField(unique=True)
+    telefono=models.CharField(max_length=50)
+    
+class Prestamo(models.Model):
+    id=models.IntegerField(auto_created=True,primary_key=True)
+    estudiante=models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    libro=models.ForeignKey(Libro,on_delete=models.CASCADE)
+    fecha=models.DateField(auto_now=True)
+    
+
+    
